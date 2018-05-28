dde98bbbfd219a708ba4e5ebcb52517ec7d0650f
a.konuhov <alexandrkonuhov@gmail.com>
2018-05-28 23:29:22 +0300

    Поменял порт сервера

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -13,7 +13,7 @@ import Vue from 'vue'
 import Axios from 'axios'
 
 Axios.defaults.headers.common.Accept = 'application/json'
-Axios.defaults.baseURL = 'http://localhost:3000'
+Axios.defaults.baseURL = 'http://91.214.130.109:3333'
 
 // Bind Axios to Vue.
 Vue.$http = Axios
